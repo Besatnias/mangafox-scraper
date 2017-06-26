c4a769ae265213fb282431528cdadad207abc6f6
Besto <manofthebag@gmail.com>
2017-06-26 18:27:18 -0400

    customisation

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+const express = require('express'),
     request = require('request'),
     cheerio = require('cheerio'),
     cors    = require('cors'),
@@ -6,9 +6,9 @@ var express = require('express'),
     path    = require('path'),
     CronJob = require('cron').CronJob;
 
-var app = express();
+const app = express();
 
-var manga = express.Router();
+const manga = express.Router();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -17,19 +17,25 @@ app.get('/', function(req, res) {
 manga.get('/all', cors(), function(req, res) {
     res.sendFile(path.join(__dirname + '/all.json'));
 });
-manga.get('/recent/:id', cors(), function(req, res) {
+
+// releases/1.htm => Latest manga releases by page (newest to oldest)
+manga.get('/releases/:id', cors(), function(req, res) {
     request('http://mangafox.me/releases/' + req.params.id + '.htm', function(err, response, body) {
 
-        var $ = cheerio.load(body);
-        var links = $('#updates .series_preview');
-        var list = [];
+        const $ = cheerio.load(body);
+        const links = $('#updates .series_preview');
+        const list = [];
+        const json = {
+            currentPage: req.params.id,
+            pageTotal: $('div#nav li:nth-child(11) > a').text()
+        }
 
         for (var i = 0; i < links.length; i++) {
-            var aElement = $(links[i]);
+            const aElement = $(links[i]);
 
-            var name = aElement.text();
-            var link = aElement.attr('href');
-            var manga_id = aElement.attr('rel');
+            const name = aElement.text();
+            const link = aElement.attr('href');
+            const manga_id = aElement.attr('rel');
 
             list.push({
                 manga_id: manga_id,
@@ -37,13 +43,47 @@ manga.get('/recent/:id', cors(), function(req, res) {
                 name: name,
                 link: link
             });
+            json.releases = list
 
         }
-        res.send(list);
+        res.send(json);
     });
 });
-manga.get('/hot/:id', cors(), function(req, res) {
-    request('http://mangafox.me/directory/' + req.params.id + '.htm', function(err, response, body) {
+
+// Everything in /directory defaults to popularity and can be modified by ?az (alphabetical), ?rating and ?latest (chapters)
+// If there are pages, it goes /directory/${modifier}/${pagenum}.htm or, if there is no modifier, /directory/${pagenum}.htm
+
+// available paths:
+
+// /directory/${genre}    => manga genres
+// /directory/${status}   => no pages, newest manga ordered by ?az ?rating ?latest and default popularity
+// /directory/${year}     => any year from 1998 to 2017 or simply /directory/older
+// /directory/${letter}   => for all manga that starts with that letter
+// /directory             => (blank) for all manga
+
+// also available:
+
+// /directory/older
+// /directory/updated
+// /directory/ongoing
+// /directory/new
+// /directory/completed
+
+// list of genres:
+// all(leave blank)
+// [ "adult", "comedy", "drama", "fantasy", "harem", "horror", "martial-arts", "mecha", "one-shot", "romance", "sci-fi",
+// "shoujo", "shounen", "slice-of-life", "sports", "tragedy", "yaoi", "action", "adventure", "doujinshi", "ecchi",
+// "gender-bender", "historical", "josei", "mature", "mystery", "psychological", "school-life", "seinen", "shoujo-ai",
+// "shounen-ai", "smut", "supernatural", "webtoons", "yuri" ]
+
+// list of alphabetical:
+// all(leave blank),
+// [ "#", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+
+// list to array text: text = "[ \"" + "# a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ").join("\", \"") + "\" ]"
+
+manga.get('/directory/:id', cors(), function(req, res) {
+    request('http://mangafox.me/directory/' + req.params.id, function(err, response, body) {
 
         var $ = cheerio.load(body);
         var links = $('.manga_text .title');
@@ -67,32 +107,9 @@ manga.get('/hot/:id', cors(), function(req, res) {
         res.send(list);
     });
 });
-manga.get('/new', cors(), function(req, res) {
-    request('http://mangafox.me/directory/new/', function(err, response, body) {
 
-        var $ = cheerio.load(body);
-        var links = $('.manga_text .title');
-        var list = [];
-
-        for (var i = 0; i < links.length; i++) {
-            var aElement = $(links[i]);
-
-            var name = aElement.text();
-            var link = aElement.attr('href');
-            var manga_id = aElement.attr('rel');
-
-            list.push({
-                manga_id: manga_id,
-                cover: 'http://a.mfcdn.net/store/manga/'+ manga_id +'/cover.jpg?',
-                name: name,
-                link: link
-            });
-
-        }
-        res.send(list);
-    });
-});
-manga.get('/show/:name', cors(), function(req, res) {
+// /details/one_piece
+manga.get('/details/:name', cors(), function(req, res) {
     request('http://mangafox.me/manga/' + req.params.name, function(err, response, body) {
 
         var $ = cheerio.load(body);
@@ -113,6 +130,9 @@ manga.get('/show/:name', cors(), function(req, res) {
             json.name = $('h1').text();
             json.summary = $('.summary').text();
         });
+        json.author = $('#title td:nth-child(2) > a').text()
+        json.artists = $('#title td:nth-child(3) > a').text()
+        json.genres = $('#title td:nth-child(4)').text().trim().replace("\r\n", "").split(', ')
 
         $('#chapters').filter(function() {
             var volume_elms = $('.volume');
@@ -153,6 +173,8 @@ manga.get('/show/:name', cors(), function(req, res) {
         res.send(json);
     });
 });
+
+// /read/one_piece/whatever/660/10 (manga: one piece, chapter: 660, pagenum: 10, volume is unnecessary, but we need to put something as filler)
 manga.get('/read/:name/:volume?/:chapter/:id', cors(), function(req, res) {
     var url = 'http://mangafox.me/manga/' + req.params.name + '/v' + req.params.volume + '/c' + req.params.chapter + '/' + req.params.id + '.html';
     request({ url: url, gzip: true }, function(err, response, body) {
@@ -170,6 +192,8 @@ manga.get('/read/:name/:volume?/:chapter/:id', cors(), function(req, res) {
             json.name = $('h1').text();
             json.title = $('strong a').text();
             json.image = $('#viewer img').attr('src');
+            json.currentPage = req.params.id
+            json.pageTotal = ($('#top_bar div > div').text()).match(/\tof ([0-9]+)\t/)[1];
         });
 
         res.send(json);
@@ -202,4 +226,4 @@ new CronJob('0 * * * *', function() {
 }, null, true, 'Asia/Tokyo');
 
 app.listen(1337);
-console.log('Running on 1337');
+console.log('Running on port 1337');
